575654936dfdf3ca0c2cd8eca38c7bf11c736ab7
koby5i <bobkoby5i@xdeq.com>
2021-02-28 00:02:56 +0100

    shared DataService

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-about',
@@ -9,12 +10,16 @@ import { Router } from '@angular/router';
 })
 export class AboutComponent implements OnInit {
 
-  constructor(private _route: ActivatedRoute, private _router: Router) { 
+  goals: any;
+
+  constructor(private _route: ActivatedRoute, 
+              private _router: Router,
+              private _data: DataService) { 
     this._route.params.subscribe(res => console.log(res.id));
   }
 
   ngOnInit(): void {
-
+    this._data.goal.subscribe(res => this.goals= res);
   }
 
   sendMeHome() {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, style, transition, animate, keyframes, query, stagger,  } from '@angular/animations';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -32,9 +33,12 @@ export class HomeComponent implements OnInit {
   btnText: string = 'Add an item'
   goalText: string =  'My first life goal';
   goals = ['sailing','skiing','scuba diving'];
-  constructor() { }
+
+  constructor(private _data: DataService) { }
 
   ngOnInit(): void {
+    this._data.goal.subscribe(res => this.goals = res);
+    this._data.changeGoal(this.goals);
     this.itemCount = this.goals.length
   }
 
@@ -42,10 +46,12 @@ export class HomeComponent implements OnInit {
     this.goals.push(this.goalText);
     this.goalText = "";
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   delItem(i) {
     this.goals.splice(i,1)
+    this._data.changeGoal(this.goals);
     this.itemCount = this.goals.length;
   }
 
