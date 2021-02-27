d2cbc074519a1779bb32370af1dacf0b8af9a4e9
koby5i <bobkoby5i@xdeq.com>
2021-02-27 23:01:52 +0100

    remove goal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,7 +14,14 @@ import { trigger, style, transition, animate, keyframes, query, stagger,  } from
             style({opacity:0,   transform:  'translateY(-75%)', offset: 0 }),
             style({opacity:0.5, transform:  'translateY(35px)', offset: .3 }),
             style({opacity:1,   transform:  'translateY(0)',    offset: 1 }),
-          ]))]),{optional: true})
+          ]))]),{optional: true}),
+
+          query(':leave', stagger('300ms', [
+            animate('.6s ease-in', keyframes([
+              style({opacity:1,   transform:  'translateY(0)', offset: 0 }),
+              style({opacity:0.5, transform:  'translateY(35px)', offset: .3 }),
+              style({opacity:0,   transform:  'translateY(-75%)',    offset: 1 }),
+            ]))]),{optional: true})          
       ])
     ]) 
   ]
@@ -34,6 +41,11 @@ export class HomeComponent implements OnInit {
   addItem() {
     this.goals.push(this.goalText);
     this.goalText = "";
+    this.itemCount = this.goals.length;
+  }
+
+  delItem(i) {
+    this.goals.splice(i,1)
     this.itemCount = this.goals.length;
   }
 
