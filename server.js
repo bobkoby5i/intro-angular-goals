1a56732fe36bfc1097c193f077a5dc136e8e9bbf
koby5i <bobkoby5i@xdeq.com>
2021-03-07 14:30:25 +0100

    node server conosle print port

--- server.js
+++ server.js
@@ -14,4 +14,6 @@ app.get('/*', function(req, res) {
  
 // 4. Listen for requests at the PORT specified by env variables 
 //    or the default Heroku port, which is 8080.
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080
+console.log('starting server port:' + port)
+app.listen(port);
